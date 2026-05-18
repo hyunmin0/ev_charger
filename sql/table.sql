990c338614e34a6c928ad6b32cde80e0e9eb3ac3
yeahR <34158074+tmxkebqoffl@users.noreply.github.com>
2026-05-18 23:36:25 +0900

    users, station 테이블 추가 및 그 외 수정
    mycar 기본키 수정
    charger method 컬럼 check 수정

--- sql/table.sql
+++ sql/table.sql
@@ -1,31 +1,72 @@
+create table users (
+	user_id uuid primary key DEFAULT gen_random_uuid(),
+	nickname varchar(50) not null,
+	profile varchar(512),
+	email varchar(255) check(email like '%@%'),
+	provider varchar(20) not null check (provider in ('kakao', 'google')),
+	provider_id varchar(255) not null,
+	refresh_token varchar(512),
+	unique(provider, provider_id)
+);
 CREATE TABLE mycar(
-    mycarid UUID NOT NULL DEFAULT gen_random_uuid(),
-    userid UUID NULL ,
-    model VARCHAR(255) NOT NULL,
-    battery_capacity FLOAT NOT NULL,
-    PRIMARY KEY (mycarid),
-    FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE SET NULL
+	mycarid UUID NOT NULL DEFAULT gen_random_uuid(),
+	userid UUID not NULL,
+	model VARCHAR(255) NOT NULL,
+	battery_capacity FLOAT NOT NULL,
+	PRIMARY KEY (mycarid, userid),
+	FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE cascade
 );
-
 CREATE TABLE favorite(
-    userid UUID NOT NULL,
-    statid  int NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (userid, statid),
-    FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE,
-    FOREIGN KEY (statid) REFERENCES station(statid) ON DELETE CASCADE
+	userid UUID NOT NULL,
+	statid int NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (userid, statid),
+	FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE,
+	FOREIGN KEY (statid) REFERENCES station(statid) ON DELETE CASCADE
 );
-
+create table station (
+	statNm varchar(100) not null,
+	statid int primary key,
+	addr varchar(150) not null,
+	addrDetail varchar(200) not null,
+	location geography(point, 4326) not null,
+	useTime varchar(50) not null,
+	busiNm varchar(50) not null,
+	busiCall varchar(20) not null,
+	zcode int not null,
+	zscode int,
+	kind varchar(2),
+	kindDetail varchar(4),
+	parkingFree varchar(1) check(parkingFree in ('Y', 'N')),
+	note varchar(200),
+	limitYn varchar(1) not null check (limitYn in ('Y', 'N')),
+	limitDetail varchar(100),
+	floorNum int,
+	floorType varchar(2) check (floorType in ('F', 'B'))
+);
 CREATE TABLE charger(
-    statid  int NOT NULL,
-    chgerid varchar(2) NOT NULL,
-    chgertype varchar(2) check (chgertype in ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10')) ,
-    stat varchar(1) check (stat in ('0', '1', '2', '3', '4', '5')) ,
-    statUpdDt varchar(14) ,
-    lastTsdt varchar(14) ,
-    lastTedt varchar(14),
-    output INTEGER,
-    method varchar(255),
-    PRIMARY KEY (statid, chgerid),
-    FOREIGN KEY (statid) REFERENCES station(statid) ON DELETE CASCADE
+	statid int NOT NULL,
+	chgerid varchar(2) NOT NULL,
+	chgertype varchar(2) check (
+		chgertype in (
+			'01',
+			'02',
+			'03',
+			'04',
+			'05',
+			'06',
+			'07',
+			'08',
+			'09',
+			'10'
+		)
+	),
+	stat varchar(1) check (stat in ('0', '1', '2', '3', '4', '5')),
+	statUpdDt varchar(14),
+	lastTsdt varchar(14),
+	lastTedt varchar(14),
+	output INTEGER,
+	method varchar(10) check(method in ('단독', '동시')),
+	PRIMARY KEY (statid, chgerid),
+	FOREIGN KEY (statid) REFERENCES station(statid) ON DELETE CASCADE
 );
