b2086b43bbd6df3510357f9a3265b9b302b1f80d
yeahR <34158074+tmxkebqoffl@users.noreply.github.com>
2026-08-04 01:50:23 +0900

    Update table.sql

--- sql/table.sql
+++ sql/table.sql
@@ -77,36 +77,36 @@ CREATE TABLE user_car(
 	FOREIGN KEY (car_id) REFERENCES car (car_id) ON DELETE cascade
 );
 create table station_operator (
-	busiId varchar(2) primary key,
-	busiNm varchar(50) not null,
-	busiCall varchar(20) not null
+	"busiId" varchar(2) primary key,
+	"busiNm" varchar(50) not null,
+	"busiCall" varchar(20) not null
 );
 create table station (
-	statNm varchar(100) not null,
-	statId varchar(8) primary key,
+	"statNm" varchar(100) not null,
+	"statId" varchar(8) primary key,
 	addr varchar(150) not null,
-	addrDetail varchar(200),
+	"addrDetail" varchar(200),
 	location geography(point, 4326) not null,
-	useTime varchar(50) not null,
-	busiId varchar(2),
+	"useTime" varchar(50) not null,
+	"busiId" varchar(2),
 	zcode varchar(2) not null,
 	zscode varchar(5),
 	kind varchar(2),
-	kindDetail varchar(4),
-	parkingFree varchar(1) check(parkingFree in ('Y', 'N')),
+	"kindDetail" varchar(4),
+	"parkingFree" varchar(1) check("parkingFree" in ('Y', 'N')),
 	note varchar(200),
-	limitYn varchar(1) check (limitYn in ('Y', 'N')),
-	limitDetail varchar(100),
-	floorNum varchar(50),
-	floorType varchar(2) check (floorType in ('F', 'B')),
-	foreign key (busiId) references station_operator(busiId) on delete
+	"limitYn" varchar(1) check ("limitYn" in ('Y', 'N')),
+	"limitDetail" varchar(100),
+	"floorNum" varchar(50),
+	"floorType" varchar(2) check ("floorType" in ('F', 'B')),
+	foreign key ("busiId") references station_operator("busiId") on delete
 	set null
 );
 CREATE TABLE charger(
-	statId varchar(8) NOT NULL,
-	chgerId varchar(2) NOT NULL,
-	chgerType char(2) not null check (
-		chgerType in (
+	"statId" varchar(8) NOT NULL,
+	"chgerId" varchar(2) NOT NULL,
+	"chgerType" char(2) not null check (
+		"chgerType" in (
 			'01',
 			'02',
 			'03',
@@ -121,13 +121,13 @@ CREATE TABLE charger(
 		)
 	),
 	stat char(1) not null check (stat in ('0', '1', '2', '3', '4', '5', '6', '9')),
-	statUpdDt char(14) not null,
-	lastTsdt char(14),
-	lastTedt char(14),
+	"statUpdDt" char(14) not null,
+	"lastTsdt" char(14),
+	"lastTedt" char(14),
 	output varchar(20),
 	method varchar(10) check(method in ('단독', '동시')),
-	PRIMARY KEY (statId, chgerId),
-	FOREIGN KEY (statId) REFERENCES station(statId) ON DELETE CASCADE
+	PRIMARY KEY ("statId", "chgerId"),
+	FOREIGN KEY ("statId") REFERENCES station("statId") ON DELETE CASCADE
 );
 create table review (
 	review_id BIGINT generated always as identity primary key,
@@ -136,11 +136,11 @@ create table review (
 	rating int not null check(
 		rating between 1 and 5
 	),
-	statId varchar(8) not null,
+	"statId" varchar(8) not null,
 	--1~5
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL,
-	foreign key (statId) references station(statId) on delete cascade,
+	foreign key ("statId") references station("statId") on delete cascade,
 	foreign key (user_id) references users(user_id) on delete cascade
 );
 create table review_image (
@@ -152,21 +152,21 @@ create table review_image (
 CREATE TABLE favorite(
 	id BIGINT generated always as identity primary key,
 	user_id UUID NOT NULL,
-	statId varchar(8) NOT NULL,
+	"statId" varchar(8) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-	unique (user_id, statId),
+	unique (user_id, "statId"),
 	FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-	FOREIGN KEY (statId) REFERENCES station(statId) ON DELETE CASCADE
+	FOREIGN KEY ("statId") REFERENCES station("statId") ON DELETE CASCADE
 );
 create table charger_alert(
 	alert_id BIGINT generated always as identity primary key,
 	user_id UUID not null,
-	statId varchar(8) not null,
-	chgerId varchar(2) not null,
+	"statId" varchar(8) not null,
+	"chgerId" varchar(2) not null,
 	created_at TIMESTAMP not null,
-	unique (user_id, statId, chgerId),
+	unique (user_id, "statId", "chgerId"),
 	FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-	foreign key (statId, chgerId) references charger(statId, chgerId) on delete cascade
+	foreign key ("statId", "chgerId") references charger("statId", "chgerId") on delete cascade
 );
 create table fcm_token(
 	id BIGINT generated always as identity primary key,
@@ -183,8 +183,8 @@ create table notice(
 );
 create table notification_history(
 	id BIGINT generated always as identity primary key,
-	statId varchar(8),
-	chgerId varchar(2),
+	"statId" varchar(8),
+	"chgerId" varchar(2),
 	notice_id BIGINT,
 	user_id UUID not null,
 	is_read boolean not null,
@@ -192,16 +192,16 @@ create table notification_history(
 	foreign key (user_id) references users (user_id) on delete cascade,
 	foreign key (notice_id) references notice(notice_id) on delete cascade,
 	check (
-		(chgerId is not null)::int + (notice_id is not null)::int = 1
+		("chgerId" is not null)::int + (notice_id is not null)::int = 1
 	) -- chger, notice 둘 중 하나는 not null이어야 함
 );
 CREATE TABLE congestion (
 	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	statId varchar(8) NOT NULL,
-	targetTime int NOT NULL,
-	congestionLevel varchar(2) NOT NULL CHECK (congestionLevel IN ('여유', '보통', '혼잡')),
-	congestionScore double precision,
-	predictedAt TIMESTAMP,
-	FOREIGN KEY (statId) REFERENCES station (statId) ON DELETE CASCADE
+	"statId" varchar(8) NOT NULL,
+	"targetTime" int NOT NULL,
+	"congestionLevel" varchar(2) NOT NULL CHECK ("congestionLevel" IN ('여유', '보통', '혼잡')),
+	"congestionScore" double precision,
+	"predictedAt" TIMESTAMP,
+	FOREIGN KEY ("statId") REFERENCES station ("statId") ON DELETE CASCADE
 );
 create index idx_station_location on station using gist(location);
